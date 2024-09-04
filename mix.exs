95163a5ce8412f83ac43d4d3cd4f33761d685bda
Nathan Caldwell <NathanCCaldwell@gmail.com>
2024-09-04 15:11:53 -0400

    add SweetXML to parse xml files

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule DocumentParser.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:sweet_xml, "~> 0.7.4"}
     ]
   end
 
