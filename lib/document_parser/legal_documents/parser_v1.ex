dc33d02795fd345c04149ccd3d5dc1f4482ac768
Nathan Caldwell <NathanCCaldwell@gmail.com>
2024-09-04 16:01:20 -0400

    add opts to Parser.V1 and @moduledoc

--- lib/document_parser/legal_documents/parser_v1.ex
+++ lib/document_parser/legal_documents/parser_v1.ex
@@ -7,12 +7,59 @@ defmodule DocumentParser.LegalDocuments.Parser.V1 do
     ~c"v."
   ]
 
-  def get_plaintiffs_and_defendants(filepath) do
+  @moduledoc """
+  Provides functions to extract and identify plaintiffs and defendants from a list of character lists or a file.
+
+  This module defines a function, `get_plaintiffs_and_defendants/2`, which takes either a list of character lists
+  or a .xml file path as input and a map containing options, and returns a map containing plaintiffs and defendants extracted from the input. The input
+  is analyzed to determine the midpoint separating plaintiffs from defendants, and the results are returned as a
+  structured map. The parsed charlists are returned for persistance purposes.
+
+  Currently supported options:
+  %{
+    plaintiff_search_breadth_override: :integer
+    defendant_search_breadth_override: :integer
+  }
+
+  The search_breadth_override options are provided to tell the parser there are more defendants or plaintiffs than it picked up, otherwise it will just find the first detected and return it.
+
+  ## Functions
+
+    * `get_plaintiffs_and_defendants/2` - Processes either a list of character lists or a file path to extract
+      plaintiffs and defendants, returning the result as a map.
+
+  ## Examples
+
+      iex> YourModuleName.get_plaintiffs_and_defendants(["MOLLY SMITH", "vs", "COMPANY LLC"], %{search_breadth_override: 5})
+      %{plaintiffs: ["MOLLY SMITH"], defendants: ["COMPANY LLC"], charlists: ["MOLLY SMITH", "vs", "COMPANY LLC"]}
+
+      iex> YourModuleName.get_plaintiffs_and_defendants("path/to/file.txt", %{})
+      %{plaintiffs: ["Plaintiff A"], defendants: ["Defendant B"], charlists: ["Plaintiff A", "Defendant B"]}
+  """
+
+  @spec get_plaintiffs_and_defendants(list(char()) | String.t(), map()) :: %{
+          plaintiffs: list(String.t()),
+          defendants: list(String.t()),
+          charlists: list(char())
+        }
+  def get_plaintiffs_and_defendants(charlists_or_filepath, opts \\ %{})
+
+  def get_plaintiffs_and_defendants(charlists, opts) when is_list(charlists),
+    do: process_charlists(charlists, opts)
+
+  def get_plaintiffs_and_defendants(filepath, opts)
+      when is_binary(filepath) do
     charlists = read_file(filepath)
+    process_charlists(charlists, opts)
+  end
+
+  defp process_charlists(charlists, opts) do
     midpoint_index = find_opponent_midpoint_index(charlists)
 
-    %{plaintiffs: _plaintiff, defendants: _defendant} =
-      find_plaintiffs_and_defendants(charlists, midpoint_index)
+    %{plaintiffs: plaintiffs, defendants: defendants} =
+      find_plaintiffs_and_defendants(charlists, midpoint_index, opts)
+
+    %{plaintiffs: plaintiffs, defendants: defendants, charlists: charlists}
   end
 
   defp read_file(filepath) do
@@ -29,16 +76,19 @@ defmodule DocumentParser.LegalDocuments.Parser.V1 do
     end)
   end
 
-  defp find_plaintiffs_and_defendants(charlists, midpoint_index) do
+  defp find_plaintiffs_and_defendants(charlists, midpoint_index, opts) do
     filter_phrases = DocumentParser.FilterPhrases.list_enabled_filter_phrases()
 
-    plaintiffs = find_opponents(charlists, midpoint_index, filter_phrases, :plaintiff)
-    defendants = find_opponents(charlists, midpoint_index, filter_phrases, :defendant)
+    plaintiff_search_breadth = Map.get(opts, :plaintiff_search_breadth_override, 1)
+    plaintiffs =
+      find_opponents(charlists, midpoint_index, filter_phrases, :plaintiff, plaintiff_search_breadth)
+
+    defendant_search_breadth = Map.get(opts, :defendant_search_breadth_override, 1)
+    defendants =
+      find_opponents(charlists, midpoint_index, filter_phrases, :defendant, defendant_search_breadth)
 
     %{plaintiffs: plaintiffs, defendants: defendants}
   end
-
-  defp find_opponents(charlists, midpoint_index, filter_phrases, opponent_type, search_breadth \\ 1)
 
   defp find_opponents(_charlists, _midpoint_index, _filter_phrases, _opponent_type, 15), do: []
 
@@ -52,8 +102,17 @@ defmodule DocumentParser.LegalDocuments.Parser.V1 do
     end)
     |> filter_scans(filter_phrases)
     |> case do
-      [] -> find_opponents(charlists, midpoint_index, filter_phrases, opponent_type, search_breadth + 1)
-      matches -> matches
+      [] ->
+        find_opponents(
+          charlists,
+          midpoint_index,
+          filter_phrases,
+          opponent_type,
+          search_breadth + 1
+        )
+
+      matches ->
+        matches
     end
   end
 
