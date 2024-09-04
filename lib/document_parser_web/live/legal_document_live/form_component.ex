4758576d1f4371e0e6679161395e91774f31124b
Nathan Caldwell <NathanCCaldwell@gmail.com>
2024-09-04 19:06:54 -0400

    add sliders to LegalDocument :new for search breadth

--- lib/document_parser_web/live/legal_document_live/form_component.ex
+++ lib/document_parser_web/live/legal_document_live/form_component.ex
@@ -2,12 +2,15 @@ defmodule DocumentParserWeb.LegalDocumentLive.FormComponent do
   use DocumentParserWeb, :live_component
 
   alias DocumentParser.LegalDocuments
+  alias DocumentParserWeb.LegalDocumentLive.SliderComponent
 
   @impl true
   def mount(socket) do
     {:ok,
      socket
-     |> allow_upload(:file, max_entries: 1, accept: ~w(.xml))}
+     |> allow_upload(:file, max_entries: 1, accept: ~w(.xml))
+     |> assign(plaintiff_search_breadth: 1)
+     |> assign(defendant_search_breadth: 1)}
   end
 
   @impl true
@@ -16,9 +19,7 @@ defmodule DocumentParserWeb.LegalDocumentLive.FormComponent do
     <div>
       <.header>
         <%= @title %>
-        <:subtitle>Upload a file</:subtitle>
       </.header>
-
 
       <.simple_form
         for={@form}
@@ -27,9 +28,24 @@ defmodule DocumentParserWeb.LegalDocumentLive.FormComponent do
         phx-change="validate"
         phx-submit="save"
       >
-        <%!-- <label for={@uploads.file}>File</label> --%>
         <.live_file_input upload={@uploads.file} />
-        <.input field={@form[:file_name]} type="text" label="File name" />
+        <.input field={@form[:file_name]} type="text" label="Document Name" />
+        <.live_component
+          module={SliderComponent}
+          field={@form[:plaintiff_search_breadth]}
+          id="plaintiff_slider"
+          value={@plaintiff_search_breadth}
+          title="Plaintiff Search Breadth"
+          name="plaintiff"
+        />
+        <.live_component
+          module={SliderComponent}
+          field={@form[:defendant_search_breadth]}
+          id="defendant_slider"
+          value={@defendant_search_breadth}
+          title="Defendant Search Breadth"
+          name="defendant"
+        />
         <:actions>
           <.button phx-disable-with="Saving...">Save Legal document</.button>
         </:actions>
@@ -50,7 +66,9 @@ defmodule DocumentParserWeb.LegalDocumentLive.FormComponent do
 
   @impl true
   def handle_event("validate", %{"legal_document" => legal_document_params}, socket) do
-    changeset = LegalDocuments.change_legal_document(socket.assigns.legal_document, legal_document_params)
+    changeset =
+      LegalDocuments.change_legal_document(socket.assigns.legal_document, legal_document_params)
+
     {:noreply, assign(socket, form: to_form(changeset, action: :validate))}
   end
 
@@ -58,8 +76,19 @@ defmodule DocumentParserWeb.LegalDocumentLive.FormComponent do
     save_legal_document(socket, socket.assigns.action, legal_document_params)
   end
 
+  def handle_event("update_slider", %{"plaintiff" => search_breadth}, socket) do
+    {:noreply, assign(socket, :plaintiff_search_breadth, search_breadth)}
+  end
+
+  def handle_event("update_slider", %{"defendant" => search_breadth}, socket) do
+    {:noreply, assign(socket, :defendant_search_breadth, search_breadth)}
+  end
+
   defp save_legal_document(socket, :edit, legal_document_params) do
-    case LegalDocuments.update_legal_document(socket.assigns.legal_document, legal_document_params) do
+    case LegalDocuments.update_legal_document(
+           socket.assigns.legal_document,
+           legal_document_params
+         ) do
       {:ok, legal_document} ->
         notify_parent({:saved, legal_document})
 
