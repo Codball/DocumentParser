06cbd4a9021a069ca6aeb32d0c0c7e9a11e09ac3
Nathan Caldwell <NathanCCaldwell@gmail.com>
2024-09-03 22:57:21 -0400

    initial test cases for Parser module

---
+++ test/document_parser/parser_test.exs
@@ -0,0 +1,37 @@
+defmodule DocumentParser.ParserTest do
+  use DocumentParser.DataCase
+
+  alias DocumentParser.LegalDocuments.Parser
+
+  @fixtures_path "/test/support/fixtures/"
+
+  test "Test A returns plaintiff and defendant" do
+    filepath = File.cwd!() <> @fixtures_path <> "A.xml"
+
+    %{plaintiffs: plaintiffs, defendants: defendants} =
+      Parser.V1.get_plaintiffs_and_defendants(filepath)
+
+    assert plaintiffs == ["ANGELO ANGELES"]
+    assert defendants == ["HILL-ROM COMPANY INC"]
+  end
+
+  test "Test B returns plaintiff and defendant" do
+    filepath = File.cwd!() <> @fixtures_path <> "B.xml"
+
+    %{plaintiffs: plaintiffs, defendants: defendants} =
+      Parser.V1.get_plaintiffs_and_defendants(filepath)
+
+    assert plaintiffs == ["KUSUMA AMBELGAR"]
+    assert defendants == ["THIRUMALLAI LLC"]
+  end
+
+  test "Test C returns plaintiff and defendant" do
+    filepath = File.cwd!() <> @fixtures_path <> "C.xml"
+
+    %{plaintiffs: plaintiffs, defendants: defendants} =
+      Parser.V1.get_plaintiffs_and_defendants(filepath)
+
+    assert plaintiffs == ["ALBA ALVARADO"]
+    assert defendants == ["LAGUARDIA ENTERPRISES INC"]
+  end
+end
